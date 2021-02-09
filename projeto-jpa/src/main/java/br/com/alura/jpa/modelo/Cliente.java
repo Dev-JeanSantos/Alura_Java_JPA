27ddfd5fa144cb7b294269cf7de428f1fb208ecc
Dev-JeanSantos <jeancbsan@gmail.com>
2021-02-09 16:35:51 -0300

    Associating categories with movements

---
+++ projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Cliente.java
@@ -0,0 +1,41 @@
+package br.com.alura.jpa.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String profissao;
+	private String endereco;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+}
--- projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
@@ -2,6 +2,7 @@ package br.com.alura.jpa.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,6 +10,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import br.com.alura.jpa.enums.TipoMovimentacao;
@@ -41,7 +43,12 @@ public class Movimentacao {
 	@ManyToOne
 	private Conta conta;
 	
-	
+
+	//MAPEAMENTO MUITOS PARA MUITOS//
+	//MUITAS MOVIMENTAÇÕES PARA UMA CONTA//
+	@ManyToMany
+	private List<Categoria> categorias;
+
 	public Long getId() {
 		return Id;
 	}
@@ -77,5 +84,8 @@ public class Movimentacao {
 	}
 	public void setConta(Conta conta) {
 		this.conta = conta;
+	}
+	public List<Categoria> getCategorias() {
+		return categorias;
 	}	
 }
